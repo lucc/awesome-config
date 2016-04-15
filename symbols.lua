9b17d982f92478224e1f0d519ef8bda3a6f79158
Lucas Hoffmann <l-m-h@web.de>
2016-04-15 16:54:27 +0200

    Add abstract names for some symbols.

---
+++ symbols.lua
@@ -0,0 +1,59 @@
+return {
+  envolope  = "\226\156\137", -- ✉
+  envolope2 = "\xef\x83\xa0", -- 
+
+  pause     = "\xe2\x8f\xb8", -- ⏸
+  pause2    = "\xef\x81\x8c", -- 
+  pause3    = "\xef\x8a\x8b", -- 
+  pause4    = "\xef\x8a\x8c", -- 
+
+  play      = "\xe2\x96\xb6", -- ▶
+  play2     = "\xef\x81\x8b",
+  --play3     = "\x",
+  --play4     = "\x",
+  play_old  = "\xe2\x8f\xb5", -- ⏵
+
+  reply     = "\xef\x84\x92", -- 
+
+  stop      = "\xe2\x97\xbc", -- ◼
+  stop2     = "\xef\x81\x8d", -- 
+  --stop3     = "\x",
+  --stop4     = "\x",
+  stop_old  = "\xe2\x8f\xb9", -- ⏹
+
+  wifi      = "\xef\x87\xab", -- 
+
+  battery0  = "\xef\x89\x84", -- 
+  battery1  = "\xef\x89\x83", -- 
+  battery2  = "\xef\x89\x82", -- 
+  battery3  = "\xef\x89\x81", -- 
+  battery4  = "\xef\x89\x80", -- 
+
+  clock0100 = "\xf0\x9f\x95\x90", -- 🕐
+  clock0200 = "\xf0\x9f\x95\x91", -- 🕑
+  clock0300 = "\xf0\x9f\x95\x92", -- 🕒
+  clock0400 = "\xf0\x9f\x95\x93", -- 🕓
+  clock0500 = "\xf0\x9f\x95\x94", -- 🕔
+  clock0600 = "\xf0\x9f\x95\x95", -- 🕕
+  clock0700 = "\xf0\x9f\x95\x96", -- 🕖
+  clock0800 = "\xf0\x9f\x95\x97", -- 🕗
+  clock0900 = "\xf0\x9f\x95\x98", -- 🕘
+  clock1000 = "\xf0\x9f\x95\x99", -- 🕙
+  clock1100 = "\xf0\x9f\x95\x9a", -- 🕚
+  clock1200 = "\xf0\x9f\x95\x9b", -- 🕛
+
+  clock0130 = "\xf0\x9f\x95\x9c", -- 🕜
+  clock0230 = "\xf0\x9f\x95\x9d", -- 🕝
+  clock0330 = "\xf0\x9f\x95\x9e", -- 🕞
+  clock0430 = "\xf0\x9f\x95\x9f", -- 🕟
+  clock0530 = "\xf0\x9f\x95\xa0", -- 🕠
+  clock0630 = "\xf0\x9f\x95\xa1", -- 🕡
+  clock0730 = "\xf0\x9f\x95\xa2", -- 🕢
+  clock0830 = "\xf0\x9f\x95\xa3", -- 🕣
+  clock0930 = "\xf0\x9f\x95\xa4", -- 🕤
+  clock1030 = "\xf0\x9f\x95\xa5", -- 🕥
+  clock1130 = "\xf0\x9f\x95\xa6", -- 🕦
+  clock1230 = "\xf0\x9f\x95\xa7", -- 🕧
+
+  emptystring = '' -- to make it easier to sort the above
+}
