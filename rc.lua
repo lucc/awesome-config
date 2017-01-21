12ad087f7a6fa935a83593d5f830e934e03adc87
Lucas Hoffmann <l-m-h@web.de>
2017-01-21 14:29:44 +0100

    Use prefered terminal again

--- rc.lua
+++ rc.lua
@@ -40,17 +40,7 @@ end
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
 
--- This is used later as the default terminal and editor to run.
-terminal = "xterm"
-editor = os.getenv("EDITOR") or "nano"
-editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+require("globals")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
