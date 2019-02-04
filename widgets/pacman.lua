29b13b2f80c91fcbafa6b0915f7b4389f0e77892
Lucas Hoffmann <lucc@posteo.de>
2019-02-04 11:38:03 +0100

    Simplify pacman module

--- widgets/pacman.lua
+++ widgets/pacman.lua
@@ -1,41 +1,28 @@
 local async = require("awful.spawn").easy_async
 local awful = require("awful")
 local gears = require("gears")
-local naughty = require("naughty")
 local pango = require("pango")
 local symbols = require("symbols")
 local wibox = require("wibox")
 
--- Pacman Widget {{{1
 -- originally copied from
 -- http://www.jasonmaur.com/awesome-wm-widgets-configuration/
-local updates = wibox.widget.textbox()
-updates.tooltip = awful.tooltip({objects={updates}})
-updates.set = function(widget, icon, tooltip)
-  widget.tooltip:set_markup(tooltip)
-  widget:set_markup(pango.iconic(icon))
+
+local pacman = wibox.widget.textbox()
+pacman.tooltip = awful.tooltip({objects={pacman}})
+
+local function set(icon, tooltip)
+  pacman.tooltip:set_markup(tooltip)
+  pacman:set_markup(pango.iconic(icon))
 end
-updates.update = function (widget)
+
+local function update()
   async({'pacman', '--query', 'linux'},
     function(stdout)
-      local installed = string.sub(stdout, 7, -2)
+      local installed = string.sub(stdout, 7, -2):gsub('-', '.')
       async({'uname', '-r'},
 	function(stdout2)
-	  local running = string.sub(stdout2, 1, installed:len())
-	  local f = function(iter)
-	    local ret = ''
-	    for x in iter do
-	      ret = ret .. '\n|'..x..'|'
-	    end
-	    return ret
-	  end
-      naughty.notify({ preset = naughty.config.presets.critical,
-		       title="debug",
-		       text='installed:' .. f(string.gmatch(installed, '[^.-]+') ) ..
-		       '\n'..
-		       'running:' ..f(string.gmatch(running, '[^.-]+') ) ..
-		       '\n'
-		     })
+	  local running = string.sub(stdout2, 1, installed:len()):gsub('-', '.')
 	  if installed == running then
 	    async({'pacman', '--query', '--upgrades'},
 	      function(text, _, _, code)
@@ -43,10 +30,10 @@ updates.update = function (widget)
 		if code == 0 then
 		  icon = pango.color('green', symbols.update2)
 		end
-		widget:set(icon, text)
+		set(icon, text)
 	    end)
 	  else
-	    widget:set(
+	    set(
 	      pango.color('red', symbols.reboot),
 	      pango('b', 'You should reboot')..'\n'..
 	      pango.color('green', 'installed kernel:\t')..installed..'\n'..
@@ -55,21 +42,25 @@ updates.update = function (widget)
       end)
   end)
 end
-updates.reboot = function() async({'reboot'}) end
-updates.ask = function(title, text, callback)
+
+local function ask(title, text)
   async({'zenity', '--question', '--title', title, '--text', text},
-	function(_, _, _, code) if code == 0 then callback() end end)
+	function(_, _, _, code) if code == 0 then async({'reboot'}) end end)
 end
-updates.timer = gears.timer{
-  timeout = 30 * 60,
-  autostart = true,
-  callback = function() updates:update() end,
-}
-updates:buttons(awful.util.table.join(
+
+pacman:buttons(awful.util.table.join(
     awful.button({}, 1, function()
-      updates.ask('Reboot now?', 'Do you want to reboot now?', updates.reboot)
+      ask('Reboot now?', 'Do you want to reboot now?', pacman.reboot)
     end
 )))
-updates:update()
 
-return updates
+local timer = gears.timer{
+  timeout = 30 * 60,
+  autostart = true,
+  callback = function() update() end,
+}
+
+-- initialize the widget
+update()
+
+return pacman
