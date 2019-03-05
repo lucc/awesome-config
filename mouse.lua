270fdf0c4fa1b3bc5b07fa1f6636971d4ce68aa9
Lucas Hoffmann <luc@artsoftandmore.com>
2019-03-05 08:55:47 +0100

    Use mous button 10 :)

--- mouse.lua
+++ mouse.lua
@@ -8,14 +8,19 @@ local mymainmenu = require("menu")
 local rootbuttons = awful.util.table.join(
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 5, awful.tag.viewprev),
+
+    awful.button({ }, 10, awful.tag.history.restore)
 )
 
 -- client mouse bindings {{{1
 local clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+    awful.button({ modkey }, 3, awful.mouse.client.resize),
+
+    awful.button({ }, 10, function() awful.tag.history.restore() end)
+    )
 
 -- return client and root buttons {{{1
 return { client = clientbuttons, root = rootbuttons }
