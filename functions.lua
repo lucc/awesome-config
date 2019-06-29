28c609c82ca709dcfc61dded542a3416f38df8d1
Lucas Hoffmann <lucc@posteo.de>
2019-06-29 10:40:22 +0200

    Add a join() function to heelpers

--- functions.lua
+++ functions.lua
@@ -6,6 +6,19 @@ local function run_in_centeral_terminal (prog)
   with_shell('env PATH=$PATH:$HOME/.config/awesome/bin term -n center -e ' .. prog)
 end
 
+local function join(array, glue)
+  local result = ''
+  if #array > 0 then
+    result = array[1]
+    for i = 2, #array do
+      result = result .. glue .. array[i]
+    end
+  end
+  return result
+end
+
+
 return {
-  run_in_centeral_terminal = run_in_centeral_terminal
+  run_in_centeral_terminal = run_in_centeral_terminal,
+  join = join
 }
