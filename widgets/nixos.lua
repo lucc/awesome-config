f0210b57591b3d4fca0788c7e6cf02d07b816cd0
Lucas Hoffmann <lucc@posteo.de>
2021-06-21 10:47:53 +0200

    Show dates instead of hashes for nixos updates

--- widgets/nixos.lua
+++ widgets/nixos.lua
@@ -2,6 +2,7 @@
 local wibox = require("wibox")
 local shell = require("awful.spawn").easy_async_with_shell
 local var_dump = require("functions").var_dump
+local json = require("lain.util.dkjson")
 local pango = require("pango")
 local symbols = require("symbols")
 local texticon = require("widgets/texticon")
@@ -9,6 +10,7 @@ local gears = require("gears")
 
 local nixos = texticon()
 local system_flake = os.getenv("HOME") .. '/src/nixos'
+local icon = pango.color("blue", pango.iconic(symbols.nixos)) .. " "
 
 local function update()
   local script = [[set -e
@@ -16,13 +18,34 @@ local function update()
 	  trap 'rm -fr "$dir"' EXIT
 	  cd "$dir"
 	  git clone --quiet --depth 1 -- file://]] .. system_flake .. [[ .
-	  nix flake update 2>&1 | grep -v '^warning: '
-	  git diff --quiet]]
+	  nix flake metadata --json
+	  nix flake update
+	  nix flake metadata --json]]
   shell(script,
     function(stdout, stderr, exitreason, exitcode)
-      if exitreason == "exit" and exitcode == 1 then
-	nixos:set_markup(pango.color("blue", pango.iconic(symbols.nixos)) .. " ")
-	nixos:set_tooltip(stdout)
+      if exitreason == "exit" and exitcode == 0 then
+	local before, pos = json.decode(stdout)
+	local after = json.decode(stdout, pos)
+	before = before.locks.nodes
+	before.root = nil
+	after = after.locks.nodes
+	local lines = {}
+	for name, info in pairs(before) do
+	  if info.locked.lastModified ~= after[name].locked.lastModified then
+	    table.insert(lines, name .. ": \t" ..
+	      os.date("%F %T", info.locked.lastModified) .. " -> " ..
+	      os.date("%F %T", after[name].locked.lastModified))
+	  end
+	end
+	if #lines > 0 then
+	  table.insert(lines, 1, pango("b", "Available Updates"))
+	  nixos:set_tooltip(table.concat(lines, "\n"), true)
+	  nixos:set_markup(icon)
+	else
+	  nixos:set_markup("")
+	end
+      else
+	nixos:set_markup("")
       end
     end)
 end
