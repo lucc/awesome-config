27df9a3e519b4abc605151b55874b3efae912407
Lucas Hoffmann <lucc@posteo.de>
2021-06-22 12:46:28 +0200

    Cleanup for debugging

--- widgets/mpd.lua
+++ widgets/mpd.lua
@@ -25,28 +25,28 @@ local function format_symbol (state)
   return pango.color(color, pango.iconic(icon)) .. ' '
 end
 
-local widget = lain.widget.mpd {
+local mpd = lain.widget.mpd {
   music_dir = '/media/nas/audio',
-  settings = function ()
+  settings = function()
     widget:set_markup(format_symbol(mpd_now.state))
   end,
 }
 
-widget.toggle = function () awful.spawn("mpc toggle"); widget.update() end
-widget.next = function () awful.spawn("mpc next"); widget.update() end
-widget.previous = function () awful.spawn("mpc prev"); widget.update() end
-widget.stop = function () awful.spawn('mpc stop'); widget.update() end
-widget.tui = function () run_in_central_terminal('ncmpcpp') end
-widget.gui = function () awful.spawn('cantata') end
+function mpd.toggle()   awful.spawn("mpc toggle") mpd.update() end
+function mpd.next()     awful.spawn("mpc next")   mpd.update() end
+function mpd.previous() awful.spawn("mpc prev")   mpd.update() end
+function mpd.stop()     awful.spawn('mpc stop')   mpd.update() end
+function mpd.tui2() run_in_central_terminal('ncmpcpp') end
+function mpd.gui() awful.spawn('cantata') end
 
-widget.widget:buttons(awful.util.table.join(
-  awful.button({ }, 1, widget.toggle),
-  awful.button({ }, 2, widget.tui),
-  awful.button({ }, 3, widget.next)
+mpd.widget:buttons(awful.util.table.join(
+  awful.button({ }, 1, mpd.toggle),
+  awful.button({ }, 2, mpd.tui),
+  awful.button({ }, 3, mpd.next)
   ))
-widget.widget:connect_signal("mouse::enter", widget.update)
+mpd.widget:connect_signal("mouse::enter", mpd.update)
 
 -- set the default icon size for mpd-notifcation(1)
 naughty.config.defaults.icon_size = 64
 
-return widget
+return mpd
